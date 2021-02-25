cbdc6cb54f5a0b365bfc705787b5057843ae0c19
Misas10 <manmisa2003@gmail.com>
2021-02-25 07:37:52 +0000

    some comments deleted

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:MrRecipe/pages/Splash_screen.dart';
 import 'package:MrRecipe/pages/user_account/login.dart';
 import 'package:MrRecipe/pages/navigation/navigation.dart';
 import 'package:MrRecipe/pages/user_account/registar.dart';
-// import 'package:MrRecipe/pages/wrapper.dart';
-// import 'package:MrRecipe/pages/welcome.dart';
 import 'package:MrRecipe/services/auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
