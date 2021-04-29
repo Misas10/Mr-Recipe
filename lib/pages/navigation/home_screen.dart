c10124733c4c8df3566c95691302079d58b1e3f0
Misas10 <manmisa2003@gmail.com>
2021-04-29 01:00:54 +0100

    deleted print on initState

--- lib/pages/navigation/home_screen.dart
+++ lib/pages/navigation/home_screen.dart
@@ -40,8 +40,6 @@ class _HomePageState extends State<HomePage>
           _scrollController.position.maxScrollExtent) {}
     });
     super.initState();
-
-    debugPrint("homePage");
   }
 
   @override
