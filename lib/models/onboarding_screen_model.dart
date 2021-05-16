f524a4cda6197ce4f9839f1f5046f5a3dea7ee24
Misas10 <manmisa2003@gmail.com>
2021-05-16 20:55:02 +0100

    first two images changed to svg

--- lib/models/onboarding_screen_model.dart
+++ lib/models/onboarding_screen_model.dart
@@ -11,14 +11,14 @@ List<OnboardingScreenModel> onboardingModels = [
     title: "Crie receitas",
     body:
         "Crie e partilhe as suas receitas saborosas com o resto do mundo e receba o feedback em tempo real.",
-    image: "assets/images/splash_screen_images/create_recipe1.jpg",
+    image: "assets/images/splash_screen_images/chef_cooking.svg",
   ),
   //
   OnboardingScreenModel(
     title: "Fácil e saudável",
     body:
         "Encontre milhares de receitas fáceis e saudáveis para que possa salvar tempo e aproveitar melhor a comida.",
-    image: "assets/images/splash_screen_images/healthy_recipe.jpg",
+    image: "assets/images/splash_screen_images/food_image.svg",
   ),
   //
   OnboardingScreenModel(
