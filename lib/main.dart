dce3c06bd24c58d4dd81e3193cb5604ccbce5212
Misas10 <manmisa2003@gmail.com>
2021-05-18 11:04:55 +0100

    useless code deleted

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:MrRecipe/pages/app.dart';
 import 'package:MrRecipe/pages/onboarding_screen.dart';
 import 'package:MrRecipe/services/auth.dart';
 import 'package:MrRecipe/widgets/widget.dart';
-import 'package:connectivity/connectivity.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -23,49 +22,12 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   bool isFirstTime;
-  StreamSubscription<ConnectivityResult> subscription;
 
   @override
   void initState() {
     getIsFirstTime();
     super.initState();
 
-    // Verifica a conexão à internet do smartphone
-    // subscription = Connectivity()
-    //     .onConnectivityChanged
-    //     .listen((ConnectivityResult result) {
-    //   debugPrint(result.toString());
-    //   if (result == ConnectivityResult.none) {
-    //     // Texto para quando não ouver conexão à internet
-    //     return showDialog(
-    //         context: context,
-    //         builder: (BuildContext context) {
-    //           return AlertDialog(
-    //             title: Text('Ooops!'),
-    //             content: SingleChildScrollView(
-    //               child: ListBody(
-    //                 children: <Widget>[
-    //                   Text("Vôce não tem uma conexão á internet."),
-    //                   Text(
-    //                       "Para uma melhor experiência estabeleça uma conexão.")
-    //                 ],
-    //               ),
-    //             ),
-    //             actions: <Widget>[
-    //               TextButton(
-    //                   onPressed: () => Navigator.of(context).pop,
-    //                   child: Text("Ok"))
-    //             ],
-    //           );
-    //         });
-    //   }
-    // });
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    subscription.cancel();
   }
 
   getIsFirstTime() async {
